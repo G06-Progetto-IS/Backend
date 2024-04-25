91a3faca37818cb6e7e82648843d7f854d4563ff
Zakka24 <114286369+Zakka24@users.noreply.github.com>
2024-04-25 23:26:18 +0200

    Collegamento al front-end API ricerca

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,8 @@
+// Stores the logged in user 
+var loggedUser = {};
+
+
 function registrati(){ 
-  var loggedUser = {};
 
   const nome = document.getElementById('nomeRegistrazione').value;
   const cognome = document.getElementById('cognomeRegistrazione').value;
@@ -7,12 +10,6 @@ function registrati(){
   const password = document.getElementById('passwordRegistrazione').value;
   const confermaPassword = document.getElementById('confermaPasswordRegistrazione').value;
   
-
-  /*console.log("Mail: ", mail)
-  console.log("Nome: ", nome)
-  console.log("Cognome: ", cognome)
-  console.log("Password: ", password)
-*/
   // Verifica che tutti i campi siano stati compilati
   if (!nome || !cognome || !mail || !password || !confermaPassword) {
     alert("Si prega di compilare tutti i campi.");
@@ -31,7 +28,7 @@ function registrati(){
       mail: mail,
       password: password
   }
-  console.log("dati: ", dati);
+  //console.log("dati: ", dati);
 
   fetch('../signUp', {
       method: 'POST',
@@ -58,10 +55,6 @@ function login(){
     return;
    }
 
-  let dati = {
-      mail: mail,
-      password: password
-  }
   console.log("dati: ", dati);
 
   fetch('../login', {
@@ -69,12 +62,90 @@ function login(){
       headers: {
           'Content-Type': 'application/json'
       },
-      body: JSON.stringify(dati)
+      body: JSON.stringify({mail: mail, password: password}),
   })
   .then((resp) => resp.json()) 
   .then(function(data) { 
+      loggedUser.token = data.token;
+      loggedUser.nome = data.nome;
+      loggedUser.cognome = data.cognome;
+      loggedUser.id = data.id;
+      
+      // Apre la pagina di homepage
       window.location.href="homepage.html"
     return;
   })
-  .catch(error => console.error(error)); // If there is any error you will catch them here
+  .catch(error => console.error(error));
 }
+
+function handle(e){
+	input=document.getElementById("searchBar").value;
+    if(e.keyCode === 13){
+    	console.log("Input: " + input);
+      ricerca()
+    }
+	return false;
+}
+
+function ricerca() {
+  var input = document.getElementById("searchBar").value;
+  
+  // Funzione per effettuare una singola richiesta di ricerca
+  function ricercaPerParametro(parametro) {
+      var url = '/ricerca?' + parametro + '=' + input;
+      console.log(url);
+      return fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              console.log(data);
+              return data;
+          });
+  }
+
+  // Effettua la ricerca per parametro=[titolo, nome, cognome]
+  ricercaPerParametro('titolo')
+      .then(titolo => {
+        if (titolo) {
+          console.log("Libro trovato: " + titolo);
+        } else {
+          // Se il titolo non è stato trovato, esegui la ricerca per il nomed dell'autore
+          return ricercaPerParametro('Author_name');
+        }
+      })
+      .then(nome => {
+        // Se è stato trovato un risultato, mostra il nome dell'autore
+        if (nome) {
+          console.log("Nome dell'autore trovato: " + nome);
+        } else {
+          // Altrimenti, effettua la ricerca per cognome
+          return ricercaPerParametro('Author_sur');
+        }
+      })
+      .then(cognome => {
+          // Se è stato trovato un risultato, mostra il cognome dell'autore
+          if (cognome) {
+            console.log("Cognome dell'autore trovato: " + cognome);
+          } else {
+            // Se non ci sono risultati per nessun parametro, mostra un avviso
+            console.log("Nessun risultato trovato.");
+          }
+      })
+      .catch(error => {
+        console.error('Si è verificato un errore durante la ricerca:', error);
+      });
+}
+
+
+
+
+// // Ricerca base, fornito sempre e solo il titolo del libro (non funziona con nome o cognome)
+// function ricerca() {
+//   const input = document.getElementById('searchBar').value;
+ 
+//       //console.log(data)
+//       fetch(`../ricerca?titolo=` + input)
+//       .then(resp => resp.json())
+//       .then(data => console.log(`Risultati per input:`, data))
+//       .catch(error => console.error(error));
+// }
+    
