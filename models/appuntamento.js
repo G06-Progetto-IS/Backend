8b3117d2c12f24b057a4ac44e0d407f8eaeb570c
Zakka24 <114286369+Zakka24@users.noreply.github.com>
2024-05-15 14:32:29 +0200

    modifiche modelli

--- models/appuntamento.js
+++ models/appuntamento.js
@@ -1,10 +1,10 @@
+// 
 const mongoose = require ("mongoose");
 
 const schema = new mongoose.Schema({
-   utente_id: {type : Number, required : true},
+   mail: {type : String, required : true},
    data : {type : Date, required : true},
    tipo_app : {type : String, required : true},
-   Stato : {type : Boolean}
 })
 
 const Appuntamento = mongoose.model("Appuntamento", schema);
--- models/utente.js
+++ models/utente.js
@@ -7,11 +7,8 @@ const schema = new mongoose.Schema({
     mail : {type : String, required : true},
     password : {type : String, required : true},
     libri_noleggiati : {type : Array, default: null},
-    n_libri : {type : Number, default: 0},
+    n_libri : {type : Number, default: null},
     autenticato : {type : Boolean, default: null},
-    data_app : {type : Date, default: null},
-    tipo_app : {type : String, default: null},
-    stato_app : {type : Boolean, default: null},
     multa : {type : Number, default: null}
 });
 
